3c0fcb9866ce13aed8434028cdbbfb358a8f2a4c
Alexander <alek.agapow80@yandex.ru>
2024-02-19 15:09:23 +0700

    home work5 внесли последние изменения

--- src/notebook/controller/UserController.java
+++ src/notebook/controller/UserController.java
@@ -55,4 +55,7 @@ public class UserController {
         String phone = UserView.prompt("Номер телефона: ");
         return new User(firstName, lastName, phone);
     }
+
+
+
 }
--- src/notebook/model/repository/impl/UserRepository.java
+++ src/notebook/model/repository/impl/UserRepository.java
@@ -1,25 +1,28 @@
 package notebook.model.repository.impl;
 
-import notebook.model.dao.impl.FileOperation;
+
 import notebook.util.UserValidator;
 import notebook.util.mapper.impl.UserMapper;
 import notebook.model.User;
 import notebook.model.repository.GBRepository;
 
+import java.io.*;
 import java.util.*;
+
+import static notebook.util.DBConnector.DB_PATH;
 
 public class UserRepository implements GBRepository {
     private final UserMapper mapper;
-    private final FileOperation operation;
+    private final String fileName;
 
-    public UserRepository(FileOperation operation) {
+    public UserRepository(String fileName) {
         this.mapper = new UserMapper();
-        this.operation = operation;
+        this.fileName = fileName;
     }
 
     @Override
     public List<User> findAll() {
-        List<String> lines = operation.readAll();
+        List<String> lines = readAll();
         List<User> users = new ArrayList<>();
         for (String line : lines) {
             users.add(mapper.toOutput(line));
@@ -106,7 +109,7 @@ public boolean delete(Long id) {
             for (User u : users) {
                 lines.add(mapper.toInput(u));
             }
-            operation.saveAll(lines);
+            saveAll(lines);
             break;
         }
 
@@ -119,7 +122,51 @@ public boolean delete(Long id) {
         for (User u : users) {
             lines.add(mapper.toInput(u));
         }
-        operation.saveAll(lines);
+        saveAll(lines);
     }
+
+
+    public List<String> readAll() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(DB_PATH);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            if (line != null) {
+                lines.add(line);
+            }
+            while (line != null) {
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                if (line != null) {
+                    lines.add(line);
+                }
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+
+    public void saveAll(List<String> data) {
+        try (FileWriter writer = new FileWriter(DB_PATH, false)) {
+            for (String line : data) {
+                // запись всей строки
+                writer.write(line);
+                // запись по символам
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
 }
